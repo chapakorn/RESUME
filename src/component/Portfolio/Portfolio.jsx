632658103c3289c9e40211f5474c7fb6e56440a0
chapakorn <chapakornfirst@gmail.com>
2024-03-06 16:34:01 +0700

    first commit

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -15,27 +15,27 @@ function Portfolio() {
       <h3 className={styles.port_title}>My Favorite Game forever</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
-            <img src={user.img1} alt="" />
+            <img src={user.img1} />
             <p>dota2: dog game.</p>
         </div>
         <div className={styles.port_items}>
-            <img src={user.img2} alt="" />
+            <img src={user.img2} />
             <p>apex: im done with this.</p>
         </div>
         <div className={styles.port_items}>
-            <img src={user.img3} alt="" />
+            <img src={user.img3} />
             <p>valorant:this too</p>
         </div>
         <div className={styles.port_items}>
-            <img src={user.img4} alt="" />
+            <img src={user.img4}  />
             <p>csgo: old but gold</p>
         </div>
         <div className={styles.port_items}>
-            <img src={user.img5} alt="" />
+            <img src={user.img5}  />
             <p>lol: dont play!</p>
         </div>
         <div className={styles.port_items}>
-            <img src={user.img6} alt="" />
+            <img src={user.img6}  />
             <p>stardew valley: best game forever</p>
         </div>
       </div>
